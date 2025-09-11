ba8c204a1b25667d8b78e6ae243caac070e94f0a
Александр <vka2@list.ru>
2025-09-11 18:11:17 +0700

    Create polybius.h

---
+++ polybius.h
@@ -0,0 +1,12 @@
+#ifndef POLYBIUS_H
+#define POLYBIUS_H
+#include <vector>
+#include <string>
+using namespace std;
+extern "C" {
+    void polybiusEncryptFile(const string& inputFile, const string& outputFile);
+    void polybiusDecryptFile(const string& inputFile, const string& outputFile);
+    string polybiusEncryptText(const string& text, bool verbose = false);
+    string polybiusDecryptText(const string& ciphtext, bool verbose = false);
+}
+#endif
