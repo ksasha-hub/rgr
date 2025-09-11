51a6278a9142050d99b370c63fa32ab98afa908c
Александр <vka2@list.ru>
2025-09-11 18:12:37 +0700

    Create gronsfeld.h

---
+++ gronsfeld.h
@@ -0,0 +1,12 @@
+#ifndef GRONSFELD_H
+#define GRONSFELD_H
+#include <vector>
+#include <string>
+using namespace std;
+extern "C" {
+    void gronsfeldEncryptFile(const string& inputFile, const string& outputFile, const string& key);
+    void gronsfeldDecryptFile(const string& inputFile, const string& key, const string& outputFile);
+    string gronsfeldEncryptText(const string& text, const string& key, bool verbose = false);
+    string gronsfeldDecryptText(const string& ciphtext, const string& key, bool verbose = false);
+}
+#endif
